806094afefc50e035a9843b5cab8da9a8f297031
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2022-01-23 15:38:39 +0900

    new project for backend using Spring Boot

---
+++ saussure-backend/src/main/java/com/ferdinand/de/SaussureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ferdinand.de;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaussureBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaussureBackendApplication.class, args);
+	}
+
+}
---
+++ saussure-backend/src/test/java/com/ferdinand/de/SaussureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferdinand.de;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaussureBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
