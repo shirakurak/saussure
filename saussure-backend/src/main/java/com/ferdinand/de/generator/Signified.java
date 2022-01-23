c5c35dec7a88375f5801e372bd473b21219285a5
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2022-01-24 07:59:42 +0900

    generator.Signifiedのtemplate作成

---
+++ saussure-backend/src/main/java/com/ferdinand/de/generator/Signified.java
@@ -0,0 +1,11 @@
+package com.ferdinand.de.generator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Signified {
+	
+	public String generate() {
+		return "synonyms and image";
+	}
+}
